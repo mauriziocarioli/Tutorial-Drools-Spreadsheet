936e4da3047bd69c06295e60a04eb3e0cd6d927e
dmAdmin <>
2018-10-16 17:03:41 -0400

    {/src/main/java/net/cloudburo/drools/model/Customer.java}

--- src/main/java/net/cloudburo/drools/model/Customer.java
+++ src/main/java/net/cloudburo/drools/model/Customer.java
@@ -31,7 +31,7 @@ public class Customer implements java.io.Serializable {
         OVER300K
     }
 
-    private net.cloudburo.drools.modelCustomerLifeStage lifestage;
+    private net.cloudburo.drools.model.Customer.CustomerLifeStage lifestage;
     private CustomerAssets assets;
     private List<CustomerNeed> customerNeeds = new ArrayList<>();
 
